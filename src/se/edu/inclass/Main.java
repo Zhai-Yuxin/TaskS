df3999dce1bc562aaa8f5332c33cbd71ab1f618f
Zhai-Yuxin <e0772744@u.nus.edu>
2022-09-16 17:24:19 +0800

    add disciplinary code

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -36,6 +36,7 @@ public class Main {
     }
 
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("print data using loop");
         for (Task t : tasksData) {
             System.out.println(t);
         }
